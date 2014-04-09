9b25c90962c0424827a099ce30dd6a0c85e0e05d
ajchuang <alfred.jenchieh.huang@gmail.com>
2014-04-09 13:17:30 -0400

    add helper functions

--- pc/pc_serv/WinServ_CloudHelper.java
+++ pc/pc_serv/WinServ_CloudHelper.java
@@ -22,11 +22,13 @@ import com.amazonaws.services.s3.model.UploadPartRequest;
 
 public class WinServ_CloudHelper {
     
-    static long sm_uploadPartSize = 500 * 1024;
+    final static String sm_bucketName = "CloudClassRoom";
+    final static long sm_uploadPartSize = 500 * 1024;
+    final static long sm_downloadPartSize = 10*1024;
     
-    static String sm_bucketName = "CloudClassRoom";
+    
     static long   sm_partSize   = 10*1024*1024;
-
+    
     // filePath: local file path
     // keyName: remove file name
     public static boolean uploadFile (String filePath, String keyName) {
@@ -99,8 +101,61 @@ public class WinServ_CloudHelper {
         return retVal;
     }
     
-    public static boolean downloadFile (String fName) {
-        return true;
-    }
+    public static boolean downloadFile (String localPath, String bucketName, String keyName) {
+        
+        // local var
+        boolean ret = true;
+        
+        /*
+        int rCounr;
+        byte[] buf = new byte[sm_downloadPartSize];
+        
+        try {
+            AmazonS3 s3Client = 
+                new AmazonS3Client (new PropertiesCredentials (
+                    LowLevelJavaUploadFile.class.getResourceAsStream ("AwsCredentials.properties")));
+                
+            S3Object remoteObj = s3Client.getObject (new GetObjectRequest (bucketName, key));
+            ObjectMetadata metaObj = s3object.getObjectMetadata ();
+             
+            WinServ.logInfo (
+                "Content-Type: " + metaObj.getContentType () + ":" + 
+                "Content-Size: " + metaObj.getContentLength ());
+                
+            // write the object to a local file.
+            InputStream reader = new BufferedInputStream (remoteObj);
+            
+            File outfile = new File (localPath);      
+            OutputStream writer = new BufferedOutputStream (new FileOutputStream (outfile));
 
+            while ((rCounr = reader.read (buf)) != -1 ) {
+                writer.write (buf);
+            }
+            
+        } catch (AmazonServiceException ase) {
+            WinServ.logErr (
+                "Caught an AmazonServiceException, which " +
+                "means your request made it " +
+                "to Amazon S3, but was rejected with an error response " +
+                "for some reason.");
+            WinServ.logEr ("Error :    " + ase.getMessage ());
+            WinServ.logEr ("HTTP Status Code: " + ase.getStatusCode ());
+            WinServ.logEr ("AWS Error Code:   " + ase.getErrorCode ());
+            WinServ.logEr ("Error Type:       " + ase.getErrorType ());
+            WinServ.logEr ("Request ID:       " + ase.getRequestId ());
+            ret = false;
+        } catch (AmazonClientException ace) {
+            WinServ.logEr (
+                "Caught an AmazonClientException, which means "+
+                "the client encountered " +
+                "an internal error while trying to " +
+                "communicate with S3, " +
+                "such as not being able to access the network.");
+            WinServ.logEr ("Error Message: " + ace.getMessage());
+            ret = false;
+        }
+        */
+        
+        return ret;
+    }
 }
