083e1659930c44f0a02e0427e4f9ac91a43d9ce5
ajchuang <alfred.jenchieh.huang@gmail.com>
2014-05-06 23:22:35 -0400

    impl add user

--- pc/pc_serv/WinServ_LoginWindow.java
+++ pc/pc_serv/WinServ_LoginWindow.java
@@ -17,7 +17,9 @@ public class WinServ_LoginWindow extends JFrame implements ActionListener, WinSe
     JPasswordField m_pwdText;
     JButton m_loginBtn;
     JButton m_cancelBtn;
+    
     JCheckBox m_signUp;
+    JCheckBox m_inst;
 
     public WinServ_LoginWindow () {
         
@@ -75,7 +77,7 @@ public class WinServ_LoginWindow extends JFrame implements ActionListener, WinSe
         c6.anchor = GridBagConstraints.CENTER;
         add (m_cancelBtn, c6);
         
-        m_loginBtn = new JButton (new String ("Login"));
+        m_loginBtn = new JButton (new String ("Ok"));
         m_loginBtn.addActionListener (this);
         GridBagConstraints c5 = new GridBagConstraints ();
         c5.gridx = 4;
@@ -88,13 +90,23 @@ public class WinServ_LoginWindow extends JFrame implements ActionListener, WinSe
         
         m_signUp = new JCheckBox ("Sing Up");
         GridBagConstraints c7 = new GridBagConstraints ();
-        c7.gridx = 4;
+        c7.gridx = 2;
         c7.gridy = 3;
         c7.gridwidth = 1;
         c7.gridheight = 1;
         c7.fill = GridBagConstraints.NONE;
         c7.anchor = GridBagConstraints.CENTER;
-        //add (m_signUp, c7);
+        add (m_signUp, c7);
+        
+        m_inst = new JCheckBox ("Instructor");
+        GridBagConstraints c8 = new GridBagConstraints ();
+        c8.gridx = 4;
+        c8.gridy = 3;
+        c8.gridwidth = 1;
+        c8.gridheight = 1;
+        c8.fill = GridBagConstraints.NONE;
+        c8.anchor = GridBagConstraints.CENTER;
+        add (m_inst, c8);
 
         pack ();
         setDefaultCloseOperation (JFrame.DO_NOTHING_ON_CLOSE);
@@ -131,7 +143,6 @@ public class WinServ_LoginWindow extends JFrame implements ActionListener, WinSe
                 if (m_signUp.isSelected ()) {
                     sendSignUpMsg (name, pwd);
                 } else {
-                    // send msg to server
                     sendLoginMsg (name, pwd);
                 }
                 
@@ -149,24 +160,28 @@ public class WinServ_LoginWindow extends JFrame implements ActionListener, WinSe
         }
     }
     
-    void sendSignUpMsg (String name, String pwd) {
-        /*
+    void sendSignUpMsg (String name, String pass) {
+        
             WinServ_ReqCommand loginCmd = new WinServ_ReqCommand ();
             
-            loginCmd.pushStr ("LOGIN_REQ");
+            loginCmd.pushStr ("NEW_USER_REQ");
             loginCmd.pushStr (":" + name);
             loginCmd.pushStr (":" + pass);
-            loginCmd.pushStr (":PC");
-            loginCmd.pushStr (":0");
+            
+            if (m_inst.isSelected ()) {
+                loginCmd.pushStr (":Instructor");
+            } else {
+                loginCmd.pushStr (":Student");
+            }
+            
             loginCmd.pushStr ("END");
             
             // REGISTER NW receiver
             WinServ_NtfServer ntfServ = WinServ_NtfServer.getNtfServ ();
-            ntfServ.registerMsgHandler ("LOGIN_RES", this);
+            ntfServ.registerMsgHandler ("NEW_USER_RES", this);
             
             // send event
             ntfServ.sendMsgToServer (loginCmd);
-        */
     }
     
     void sendLoginMsg (String name, String pass) {
@@ -201,60 +216,84 @@ public class WinServ_LoginWindow extends JFrame implements ActionListener, WinSe
         m_isAutheticating = false;
         
         String ln1 = cmd.getStrAt (0);
-    
-        if (ln1.equals ("LOGIN_RES") == false) {
+        
+        if (ln1.equals ("NEW_USER_RES")) {
+            String ln2 = cmd.getStrAt (1).substring (1);
+            
+            if (ln2.equals ("SUCCESS")) {
+                String name = m_nameText.getText();
+                String pwd = new String (m_pwdText.getPassword ());
+                sendLoginMsg (name, pwd);
+            } else {
+                JOptionPane.showMessageDialog (
+                    this,
+                    "Sign-up failed. Try again. Reason: " + ln2,
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE,
+                    new ImageIcon (WinServ_SysParam.gtResPath ("error.png")));
+                    
+                // re-enable the login box
+                m_loginBtn.setEnabled   (true);
+                m_cancelBtn.setEnabled  (true);
+                m_nameText.setEditable  (true);
+                m_pwdText.setEditable   (true);
+            }
+        } else if (ln1.equals ("LOGIN_RES")) {
+            String ln2 = cmd.getStrAt (1).substring (1);
+            String ln3 = cmd.getStrAt (2).substring (1);
+            String ln4 = cmd.getStrAt (3).substring (1);
+        
+        
+            if (ln2.equals ("LOGGED_IN") || ln2.equals ("DUPLICATE")) {
+                int cookieId = Integer.parseInt (ln3);
+                
+                // update repo
+                WinServ_DataRepo repo = WinServ_DataRepo.getDataRepo ();
+                repo.setLoggedIn (true);
+                repo.setCookieId (cookieId);
+                
+                if (ln4.equals ("Instructor")) {
+                    WinServ.logInfo ("I am inst");
+                    repo.setInstructor (true);
+                    repo.setPresenter (true);
+                } else {
+                    WinServ.logInfo ("I am student");
+                    repo.setInstructor (false);
+                    repo.setPresenter (false);
+                }
+                
+                // clear the message registration
+                WinServ_NtfServer ntfServ = WinServ_NtfServer.getNtfServ ();
+                ntfServ.unregisterMsgHandler ("LOGIN_RES", this);
+            
+                // create new window, and close the login window
+                WinServ_ControlPanel ctrlPanel = WinServ_ControlPanel.getCtrlPanel ();
+                dispose ();
+                
+            } else {
+                // error information
+                WinServ.logInfo ("Login failed: " + ln2);
+                
+                JOptionPane.showMessageDialog (
+                    this,
+                    "Login failed. Try again.",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE,
+                    new ImageIcon (WinServ_SysParam.gtResPath ("error.png")));
+                
+                // re-enable the login box
+                m_loginBtn.setEnabled   (true);
+                m_cancelBtn.setEnabled  (true);
+                m_nameText.setEditable  (true);
+                m_pwdText.setEditable   (true);
+            }
+        } else {
             WinServ.logErr ("Bad response - " + ln1);
             System.exit (0);
         }
-        
-        String ln2 = cmd.getStrAt (1).substring (1);
-        String ln3 = cmd.getStrAt (2).substring (1);
-        String ln4 = cmd.getStrAt (3).substring (1);
+    
         
         
-        if (ln2.equals ("LOGGED_IN") || ln2.equals ("DUPLICATE")) {
-            int cookieId = Integer.parseInt (ln3);
-            
-            // update repo
-            WinServ_DataRepo repo = WinServ_DataRepo.getDataRepo ();
-            repo.setLoggedIn (true);
-            repo.setCookieId (cookieId);
-            
-            if (ln4.equals ("Instructor")) {
-                WinServ.logInfo ("I am inst");
-                repo.setInstructor (true);
-                repo.setPresenter (true);
-            } else {
-                WinServ.logInfo ("I am student");
-                repo.setInstructor (false);
-                repo.setPresenter (false);
-            }
-            
-            // clear the message registration
-            WinServ_NtfServer ntfServ = WinServ_NtfServer.getNtfServ ();
-            ntfServ.unregisterMsgHandler ("LOGIN_RES", this);
-        
-            // create new window, and close the login window
-            WinServ_ControlPanel ctrlPanel = WinServ_ControlPanel.getCtrlPanel ();
-            dispose ();
-            
-        } else {
-            // error information
-            WinServ.logInfo ("Login failed: " + ln2);
-            
-            JOptionPane.showMessageDialog (
-                this,
-                "Login failed. Try again.",
-                "Error",
-                JOptionPane.ERROR_MESSAGE,
-                new ImageIcon (WinServ_SysParam.gtResPath ("error.png")));
-            
-            // re-enable the login box
-            m_loginBtn.setEnabled   (true);
-            m_cancelBtn.setEnabled  (true);
-            m_nameText.setEditable  (true);
-            m_pwdText.setEditable   (true);
-        }
         
         
         
