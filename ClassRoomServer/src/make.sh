53ccccd084d1950b023f1cab705e1c1a32998b9e
Ubuntu <ubuntu@ip-172-31-2-207.us-west-2.compute.internal>
2014-05-10 06:13:46 +0000

    revise scripts

--- ClassRoomServer/src/make.sh
+++ ClassRoomServer/src/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-javac ./server/*.java
-javac ./message/*.java
+javac -cp ./:../JavaPNS_2.2.jar:../bcprov-jdk15on-150.jar:../log4j-1.2.17.jar:../sqlite-jdbc-3.7.2.jar ./server/*.java
+javac -cp ./:../JavaPNS_2.2.jar:../bcprov-jdk15on-150.jar:../log4j-1.2.17.jar:../sqlite-jdbc-3.7.2.jar ./message/*.java
--- ClassRoomServer/src/run.sh
+++ ClassRoomServer/src/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cp ../CloudClassRoom.db ./
-java -classpath ../sqlite-jdbc-3.7.2.jar:./ server/Server 4119 > log.txt
+java -classpath ./:../JavaPNS_2.2.jar:../bcprov-jdk15on-150.jar:../log4j-1.2.17.jar:../sqlite-jdbc-3.7.2.jar server/Server 4119 > log.txt
